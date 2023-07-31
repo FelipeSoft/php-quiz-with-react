ad5753c1a128d8726450c454ac1d3d8aeee19bd7
FelipeSoft <felipedecastrolima2@gmail.com>
2023-07-31 11:54:46 -0300

    working in fix bug

--- src/app/components/Answer.tsx
+++ src/app/components/Answer.tsx
@@ -1,20 +1,29 @@
 type Props = {
     label: string;
+    className: string;
+    border: string;
     selected: boolean;
     validator: boolean | 0;
+    isFirst: boolean;
     onClick: (e: React.SyntheticEvent<HTMLDivElement>) => void;
-}
+};
+  
+export const Answer = ({ border, className, selected, label, onClick, validator = 0, isFirst }: Props) => {
+const borderColor = isFirst && validator === false ? "border-red-800" : "border-blue-950";
 
-export const Answer = ({ selected, label, onClick, validator = 0}: Props) => {
-    return (
-        <div 
-            onClick={onClick} 
-            className={`cursor-pointer w-full h-10 border-2 border-blue-950 rounded-xl text-white flex items-center justify-start mb-4 px-4 hover:bg-slate-800 hover:border-slate-800
-            ${validator === 0 ? "bg-slate-900" : ""}
-            ${selected && validator && "bg-green-800"}
-            ${selected && !validator && "bg-red-800"} 
-            `}>
-            { label }
-        </div>
-    );
-}
+return (
+    <div
+    onClick={onClick}
+    className={`${border} ${className} cursor-pointer w-full h-10 border-2 rounded-xl text-white flex items-center justify-start mb-4 px-4
+        ${validator === 0 ? "bg-slate-900" : ""}
+        ${selected && validator ? "bg-green-800 border-green-800" : ""}
+        ${selected && !validator ? "bg-red-800 border-red-800" : ""}
+        ${!selected ? "bg-slate-900" : ""}
+        ${borderColor}
+    `}
+    >
+    {label}
+    </div>
+);
+};
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,40 +4,38 @@ import { questions } from "@/app/data/questions";
 import { Question } from "./components/Question";
 import { Answer } from "@/app/components/Answer";
 import { AnswerType } from "@/app/types/AnswerType";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Page = () => {
-    const [selectedAnswerIndex, setSelectedAnswerIndex] = useState<AnswerType>({index: 0, status: 0});
-    const [isInteractable, setIsInteractable] = useState<boolean>(true);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState<AnswerType>({ index: 0, status: 0 });
+  const [isInteractable, setIsInteractable] = useState<boolean>(true);
 
-    const handleClick = (event: React.SyntheticEvent<HTMLDivElement>, index: number) => {
-        if (!isInteractable) return;
+  const handleClick = (event: React.SyntheticEvent<HTMLDivElement>, index: number) => {
+    if (!isInteractable) return;
+    let status = index === questions[0].correct;
 
-        setSelectedAnswerIndex({index: index, status: 0});
+    setSelectedAnswerIndex({ index: index, status: status });
+    setIsInteractable(false);
+  };
 
-        if(index === questions[0].correct){
-            setSelectedAnswerIndex({index: index, status: true});
-        } else {
-            setSelectedAnswerIndex({index: index, status: false});
-        }
-
-        setIsInteractable(false);
-    }
-
-    return (
-        <div className="container mx-auto w-screen min-h-screen flex flex-col items-center justify-center">
-            <Question label={questions[0].question} numberOfQuestions={questions.length} currentQuestion={1}>
-                {questions[0].answers.map((element, index) => 
-                    <Answer
-                        validator={selectedAnswerIndex.status}
-                        selected={selectedAnswerIndex.index === index}
-                        onClick={(event) => handleClick(event, index)} 
-                        label={element}
-                    />
-                )}
-            </Question>
-        </div>
-    );
-}
+  return (
+    <div className="container mx-auto w-screen min-h-screen flex flex-col items-center justify-center">
+      <Question label={questions[0].question} numberOfQuestions={questions.length} currentQuestion={1}>
+        {questions[0].answers.map((element, index) => (
+          <Answer
+            key={index}
+            border={`border-blue-950`}
+            validator={selectedAnswerIndex.status}
+            selected={selectedAnswerIndex.index === index}
+            onClick={(event) => handleClick(event, index)}
+            label={element}
+            className={!isInteractable ? "disable-hover" : ""}
+            isFirst={index === 0}
+          />
+        ))}
+      </Question>
+    </div>
+  );
+};
 
 export default Page;
