ae3671eec2aedbcae50ed8386588acc825cedf14
FelipeSoft <felipedecastrolima2@gmail.com>
2023-07-31 22:32:35 -0300

    adicionando mensagens finais

--- src/app/components/Result.tsx
+++ src/app/components/Result.tsx
@@ -8,6 +8,12 @@ type Props = {
 
 export const Result = ( { enable, correct, incorrect }: Props ) => {
     let message = "";
+    if(correct <= 2) message = "Você é burro, burro, burro!"
+    else if(correct > 2 && correct <= 4) message = "Meu nome é Ari e eu não tô nem aí!"
+    else if(correct > 4 && correct <= 6) message = "Dá pro gasto..."
+    else if(correct > 6 && correct <= 8) message = "Lembra muito meu começo!"
+    else if(correct > 8 && correct <= 9) message = "Freelancer profissional"
+    else if(correct = 10) message = "Felipão, é você?!"
 
     return (
         <div className={`
