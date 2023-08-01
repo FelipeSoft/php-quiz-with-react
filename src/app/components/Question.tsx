c150ca05869dcf055b9210132c9bbb0b0739da56
FelipeSoft <felipedecastrolima2@gmail.com>
2023-08-01 10:31:20 -0300

    adicionando imagens

--- src/app/components/Question.tsx
+++ src/app/components/Question.tsx
@@ -15,15 +15,15 @@ export const Question = ({ onClick, enable, enableQuestion, children, label, num
     return (
         <div className={`max-h-full w-full ${enableQuestion && "block"} ${!enableQuestion && "hidden"}`}>
             <p className="text-gray-500 text-xs">{currentQuestion} / {numberOfQuestions}</p>
-            <h1 className="text-white mb-4 text-sm">{ label }</h1>
+            <h1 className="text-white text-sm">{ label }</h1>
             <div className="w-full h-auto">
-                <img className={`w-full h-auto`} src={`/assets/images/ ${image}`} alt="" />
+                <img className={`w-full h-auto my-4`} src={`../assets/images/${image}`} alt="" />
             </div>
             <div className="flex flex-col">
                 { children }
             </div>
             <button 
-                className={`w-full h-max px-4 py-2 bg-blue-800 rounded-md text-white text-xs mt-4 ${enable && "block pointer-events-auto"} ${!enable && "opacity-0 pointer-events-none"}`} 
+                className={`w-full h-max px-4 py-2 bg-blue-800 rounded-md text-white text-xs ${enable && "block pointer-events-auto"} ${!enable && "opacity-0 pointer-events-none"}`} 
                 onClick={onClick}>Next</button>
         </div>
     );
