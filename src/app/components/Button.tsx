9a8593e05a2988815bf823085e343d86685b2c2d
FelipeSoft <felipedecastrolima2@gmail.com>
2023-08-04 10:53:46 -0300

    fixing bugs

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -9,7 +9,7 @@ export const Button = ({ enable, backgroundColor, label, onClick }: Props) => {
     return (
         <button onClick={onClick} className={`w-full h-max px-4 py-2 ${backgroundColor} rounded-md text-white text-xs 
         ${enable && "block pointer-events-auto"} 
-        ${!enable && "opacity-0 pointer-events-none"}`}>
+        ${!enable && "opacity-0 none pointer-events-none"}`}>
         { label }
         </button>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,125 +1,104 @@
 "use client";
 
+import { useState } from "react";
 import { questions } from "@/app/data/questions";
 import { Question } from "./components/Question";
 import { Answer } from "@/app/components/Answer";
-import { AnswerType } from "@/app/types/AnswerType";
-import { useState } from "react";
 import { Result } from "@/app/components/Result";
 import { Points } from "@/app/types/Points";
 import { ProgressBar } from "@/app/components/ProgressBar";
 
 const Page = () => {
-  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState<AnswerType>({ index: -1, status: 0 });
-  const [isInteractable, setIsInteractable] = useState<boolean>(true);
-  const [enable, setEnable] = useState({ enableButtonNext: false, enableButtonConfirm: false })
-  const [current, setCurrent] = useState<number>(0)
-  const [enableResult, setEnableResult] = useState<boolean>(false);
-  const [points, setPoints] = useState<Points>({correct: 0, incorrect: 0});
-  const [enableQuestion, setEnableQuestion] = useState<boolean>(true);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState({ index: -1, status: 0 });
+  const [isInteractable, setIsInteractable] = useState(true);
+  const [enable, setEnable] = useState({ enableButtonNext: false, enableButtonConfirm: false });
+  const [current, setCurrent] = useState(0);
+  const [enableResult, setEnableResult] = useState(false);
+  const [points, setPoints] = useState<Points>({ correct: 0, incorrect: 0 });
   const [showCorrect, setShowCorrect] = useState<number | null>(null);
-  const [progress, setProgress] = useState<number>(10);
-  const [progressPoints, setProgressPoints] = useState<number>(2);
-  const [enableProgress, setEnableProgress] = useState<boolean>(true);
+  const [progressPoints, setProgressPoints] = useState(1);
   const [selected, setSelected] = useState(-1);
 
   const handleSelect = (index: number) => {
-    if(!isInteractable) return;
+    if (!isInteractable) return;
     setSelected(index);
-    setSelectedAnswerIndex({...selectedAnswerIndex, index: index})
-    setEnable({...enable, enableButtonConfirm: true})
-  }
+    setSelectedAnswerIndex({ index: index, status: 0 });
+    setEnable({ ...enable, enableButtonConfirm: true });
+  };
 
-  const handleConfirm = (index: number) => {
+  const handleConfirm = () => {
     setIsInteractable(false);
+    const status = selectedAnswerIndex.index === questions[current].correct ? 1 : 0;
 
-    if (!isInteractable && index === null) return;
-    let status = index === questions[current].correct;
-
-    if(status){
-      setPoints({
-        ...points,
-        correct: points.correct + 1
-      });
-      setSelectedAnswerIndex({...selectedAnswerIndex, status: true });
-    } else if (!status) {
-      setShowCorrect(questions[current].correct)
-      setSelectedAnswerIndex({ index: index, status: 0 })
+    if (status) {
+      setPoints((prevPoints) => ({ ...prevPoints, correct: prevPoints.correct + 1 }));
+    } else {
+      setPoints((prevPoints) => ({ ...prevPoints, incorrect: prevPoints.incorrect + 1 }));
+      setShowCorrect(questions[current].correct);
     }
-    
-    alert(`Selecionada: ${selected}, Confirmada: ${selectedAnswerIndex.index}, Correta? ${selectedAnswerIndex.status === false ? "Sim" : "Não"}`)
 
-    setSelected(-1);
+    setSelectedAnswerIndex({ index: selected, status: status });
     setIsInteractable(false);
-    setEnable({...enable, enableButtonNext: true});
+    setEnable({ enableButtonNext: true, enableButtonConfirm: false });
     setEnableResult(false);
-  }
+  };
 
   const handleNext = () => {
-    if(current < questions.length){
-      if(selectedAnswerIndex.index === questions[current].correct){
-        setPoints({
-          ...points,
-          correct: points.correct + 1
-        })
-      } else {
-        setPoints({
-          ...points,
-          incorrect: points.incorrect + 1
-        })
-      }
-      setEnable({enableButtonConfirm: false, enableButtonNext: false});
-      setIsInteractable(true);
-      setShowCorrect(null);
-      setSelectedAnswerIndex({ index: -1, status: 0 }); 
-      setSelected(-1);
+    setEnable({ enableButtonNext: false, enableButtonConfirm: false });
+    setIsInteractable(true);
+    setShowCorrect(null);
+    setSelectedAnswerIndex({ index: -1, status: 0 });
+    setProgressPoints((prevPoints) => prevPoints + 1);
+    setCurrent((prevCurrent) => prevCurrent + 1);
 
-      setProgressPoints(progressPoints + 1);
-      setProgress(( progressPoints / questions.length ) * 100);
-      setCurrent(current + 1);
-    } else {
+    if (current >= questions.length - 1) {
       setEnableResult(true);
-      setEnableQuestion(false);
-      setEnableProgress(false);
       setIsInteractable(false);
-    } 
-  }
+    }
+  };
+
+  const progress = (progressPoints / questions.length) * 100;
+  const enableProgress = current < questions.length;
 
   return (
     <div className="relative container mx-auto max-w-lg min-h-screen flex flex-col items-center justify-center p-4">
-      <ProgressBar width={progress} enable={enableProgress}/>
+      <ProgressBar width={progress} enable={enableProgress} />
       <div className="relative w-full h-full flex flex-col justify-center items-center">
-        <Question   
-          enableQuestion={enableQuestion}
-          label={questions[current].question} 
-          image={questions[current].assistant}
-          numberOfQuestions={questions.length} 
-          currentQuestion={current + 1}
-          enableButtonConfirm={enable.enableButtonConfirm}
-          enableButtonNext={enable.enableButtonNext}
-          onClick1={()=>handleConfirm(selectedAnswerIndex.index)}
-          onClick2={handleNext}
+        {current < questions.length && (
+          <Question
+            enableQuestion={true}
+            label={questions[current].question}
+            image={questions[current].assistant}
+            numberOfQuestions={questions.length}
+            currentQuestion={current + 1}
+            enableButtonConfirm={enable.enableButtonConfirm}
+            enableButtonNext={enable.enableButtonNext}
+            onClick1={handleConfirm}
+            onClick2={handleNext}
           >
-          {questions[current].answers.map((element, index) => (
-            <Answer
-              standard={!(selectedAnswerIndex.index === index) && !(showCorrect === index)}
-              isCorrect={showCorrect === index}
-              key={index}
-              confirmed={selectedAnswerIndex.index === index}
-              validator={selectedAnswerIndex.index === questions[current].correct}
-              onClick={() => handleSelect(index)}
-              label={element}
-              className={!isInteractable ? "disable-hover" : ""}
-              isSelected={selected === index}
-            />
-          ))}
-        </Question>
-        <Result 
-          enable={enableResult}
-          correct={points.correct}
-          incorrect={points.incorrect}
-          total={points.correct + points.incorrect}
-        />
+            {questions[current].answers.map((element, index) => (
+              <Answer
+                key={index}
+                standard={!(selectedAnswerIndex.index === index) && !(showCorrect === index)}
+                isCorrect={showCorrect === index}
+                confirmed={selectedAnswerIndex.index === index}
+                validator={selectedAnswerIndex.index === questions[current].correct}
+                onClick={() => handleSelect(index)}
+                label={element}
+                className={!isInteractable ? "disable-hover" : ""}
+                isSelected={selectedAnswerIndex.index === index}
+              />
+            ))}
+          </Question>
+        )}
+        {enableResult && (
+          <Result
+            correct={points.correct}
+            incorrect={points.incorrect}
+            total={points.correct + points.incorrect}
+            enable={enableResult}
+          />
+        )}
       </div>
     </div>
   );
